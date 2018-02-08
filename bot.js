a64a3bd668f19df8d339e79c3be82caac65b9f2c
A1asd <iamhldisch@hotmail.de>
2018-02-08 14:26:23 +0100

    changed various stuff a while ago. i dont even know

--- bot.js
+++ bot.js
@@ -10,25 +10,39 @@ client.on('ready', () => {
 });
 
 client.on('guildMemberAdd', (member) => {
-	message.channel.send("Hello "+member.user);
+	member.guild.defaultChannel.send("Hello "+member.user);
 });
 
 client.on('guildMemberRemove', (member) => {
-	message.channel.send("We will miss you "+member.user);
+	member.guild.defaultChannel.send("We will miss you "+member.user);
 });
 
 client.on('message', message => {
-	if (message.content === 'addme'){
-		let role = message.guild.roles.find("name", "Administrator");
-		let member = message.member;
-		member.addRole(role).catch(console.error);
-		message.channel.send("added you");
+	if (message.author.bot) return;
+	var splitmessage = message.content.split(" ");
+	var command = splitmessage[0];
+//	for (i=0; i<splitmessage.length; i++){
+//		message.channel.send(i+"ter command: "+splitmessage[i]);
+//	}
+	if (command === 'join'){
+		if (splitmessage.length > 1){
+			let role = message.guild.roles.find("name", splitmessage[1]);
+			let member = message.member;
+			member.addRole(role).catch(console.error);
+			message.channel.send("You joined "+role);
+		} else {
+			message.channel.send("ich brauche mehr argumente")
+		}
 	}
-	if (message.content === 'remme'){
-		let role = message.guild.roles.find("name", "Administrator");
-		let member = message.member;
-		member.removeRole(role).catch(console.error);
-		message.channel.send("removed you");
+	if (command === 'leave'){
+		if (splitmessage.length > 1){
+			let role = message.guild.roles.find("name", splitmessage[1]);
+			let member = message.member;
+			member.removeRole(role).catch(console.error);
+			message.channel.send("You left "+role);
+		} else {
+			message.channel.send("Gib mir diese Argumente");
+		}
 	}
 	if (message.content === 'yp'){
 		let perms = message.channel.permissionsFor(message.member);
